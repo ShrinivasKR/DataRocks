a3ac58c99809f369b7a7aa83c8032b0b68dff29f
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-05-28 04:59:31 -0700

    All code in app.js, node command successfully communicates with DataBase

---
+++ app.js
@@ -0,0 +1,77 @@
+var sql = require('mssql');
+
+var config = {
+    server: 'IS-HAY04.ischool.uw.edu',
+    database: 'DataRocks',
+    user: 'INFO445',
+    password: 'GoHuskies!',
+    port: 1433
+};
+
+function query() {
+    var dbConn = new sql.Connection(config);
+    dbConn.connect().then(function () {
+        var request = new sql.Request(dbConn);
+        request.query("select * from Inventory").then(function (recordSet) {
+            console.log(recordSet);
+            dbConn.close();
+        }).catch(function (err) {
+            console.log(err);
+            dbConn.close();
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function executeStoredProc() {
+    var dbConn = new sql.Connection(config);
+    dbConn.connect().then(function () {
+
+        var request = new sql.Request(dbConn);
+        request.input('NumRows', sql.Int, 1)
+            .execute("uspPopulateIncidents").then(function (recordSet) {
+                console.log(recordSet);
+                dbConn.close();
+            }).catch(function (err) {
+                console.log(err);
+                dbConn.close();
+            });
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function insertRow() {
+    var dbConn = new sql.Connection(config);
+    dbConn.connect().then(function () {
+        var transaction = new sql.Transaction(dbConn);
+        transaction.begin().then(function () {
+            var request = new sql.Request(transaction);
+            request.query("Insert into EmployeeInfo (EmpName,Salary,DeptName,Designation) values ('T.M. Sabnis',13000,'Accounts','Lead')")
+                .then(function () {
+                    transaction.commit().then(function (recordSet) {
+                        console.log(recordSet);
+                        dbConn.close();
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Commit " + err);
+                        dbConn.close();
+                    });
+                }).catch(function (err) {
+                    console.log("Error in Transaction Begin " + err);
+                    dbConn.close();
+                });
+
+        }).catch(function (err) {
+            console.log(err);
+            dbConn.close();
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+
+query();
+// executeStoredProc();
+// insertRow();

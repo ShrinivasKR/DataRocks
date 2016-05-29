5273bd6cf7c3b9960e512c14f7e9097cce05ef83
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-05-29 14:04:44 -0700

    factored sql connections into serverRoutes.js from server.js

--- app/serverRoutes.js
+++ app/serverRoutes.js
@@ -1,7 +1,11 @@
 // app/routes.js
 
 // grab the nerd model we just created
+var config = require('../config/db.js');
 
+// connect to our mssql database 
+var sql = require('mssql');
+var dbConn = new sql.Connection(config);
 
 module.exports = function (app) {
 
@@ -13,10 +17,78 @@ module.exports = function (app) {
     // route to handle creating goes here (app.post)
     // route to handle delete goes here (app.delete)
 
+    app.get('/api/motorcycles', function (req, res) {
+        dbConn.connect().then(function () {
+            var request = new sql.Request(dbConn);
+            request.query("select * from Inventory").then(function (recordSet) {
+                console.log(recordSet);
+                dbConn.close();
+            }).catch(function (err) {
+                console.log(err);
+                dbConn.close();
+            });
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+    });
+
+
+    function executeStoredProc() {
+        dbConn.connect().then(function () {
+
+            var request = new sql.Request(dbConn);
+            request.input('NumRows', sql.Int, 1)
+                .execute("uspPopulateIncidents").then(function (recordSet) {
+                    console.log(recordSet);
+                    dbConn.close();
+                }).catch(function (err) {
+                    console.log(err);
+                    dbConn.close();
+                });
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+    function insertRow() {
+        dbConn.connect().then(function () {
+            var transaction = new sql.Transaction(dbConn);
+            transaction.begin().then(function () {
+                var request = new sql.Request(transaction);
+                request.query("Insert into EmployeeInfo (EmpName,Salary,DeptName,Designation) values ('T.M. Sabnis',13000,'Accounts','Lead')")
+                    .then(function () {
+                        transaction.commit().then(function (recordSet) {
+                            console.log(recordSet);
+                            dbConn.close();
+                        }).catch(function (err) {
+                            console.log("Error in Transaction Commit " + err);
+                            dbConn.close();
+                        });
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Begin " + err);
+                        dbConn.close();
+                    });
+
+            }).catch(function (err) {
+                console.log(err);
+                dbConn.close();
+            });
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+
+    executeStoredProc();
+    // executeStoredProc();
+    // insertRow();
     // frontend routes =========================================================
     // route to handle all angular requests
     app.get('*', function (req, res) {
-        res.sendFile('www/index.html', { root: __dirname + '/..'}); // load our public/index.html file
+        res.sendFile('www/index.html', { root: __dirname + '/..' }); // load our public/index.html file
     });
+
+    app.get('')
 
 };
--- server.js
+++ server.js
@@ -13,9 +13,6 @@ var port = process.env.PORT || 8080;
 // set our port
 var port = process.env.PORT || 8080;
 
-// connect to our mssql database 
-var sql = require('mssql');
-
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
 app.use(bodyParser.json());
@@ -44,71 +41,3 @@ console.log('Magic happens on port ' + port);
 
 // expose app           
 exports = module.exports = app;
-
-function query() {
-    var dbConn = new sql.Connection(config);
-    dbConn.connect().then(function () {
-        var request = new sql.Request(dbConn);
-        request.query("select * from Inventory").then(function (recordSet) {
-            console.log(recordSet);
-            dbConn.close();
-        }).catch(function (err) {
-            console.log(err);
-            dbConn.close();
-        });
-    }).catch(function (err) {
-        console.log(err);
-    });
-}
-
-function executeStoredProc() {
-    var dbConn = new sql.Connection(config);
-    dbConn.connect().then(function () {
-
-        var request = new sql.Request(dbConn);
-        request.input('NumRows', sql.Int, 1)
-            .execute("uspPopulateIncidents").then(function (recordSet) {
-                console.log(recordSet);
-                dbConn.close();
-            }).catch(function (err) {
-                console.log(err);
-                dbConn.close();
-            });
-    }).catch(function (err) {
-        console.log(err);
-    });
-}
-
-function insertRow() {
-    var dbConn = new sql.Connection(config);
-    dbConn.connect().then(function () {
-        var transaction = new sql.Transaction(dbConn);
-        transaction.begin().then(function () {
-            var request = new sql.Request(transaction);
-            request.query("Insert into EmployeeInfo (EmpName,Salary,DeptName,Designation) values ('T.M. Sabnis',13000,'Accounts','Lead')")
-                .then(function () {
-                    transaction.commit().then(function (recordSet) {
-                        console.log(recordSet);
-                        dbConn.close();
-                    }).catch(function (err) {
-                        console.log("Error in Transaction Commit " + err);
-                        dbConn.close();
-                    });
-                }).catch(function (err) {
-                    console.log("Error in Transaction Begin " + err);
-                    dbConn.close();
-                });
-
-        }).catch(function (err) {
-            console.log(err);
-            dbConn.close();
-        });
-    }).catch(function (err) {
-        console.log(err);
-    });
-}
-
-
-query();
-// executeStoredProc();
-// insertRow();

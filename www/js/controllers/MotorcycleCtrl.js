5b3e2dae0c2b43751e2b365d917b38fb0523e579
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-05-31 23:22:42 -0700

    Now populates all autofill models in one foreach loop

--- www/js/controllers/MotorcycleCtrl.js
+++ www/js/controllers/MotorcycleCtrl.js
@@ -7,27 +7,20 @@ angular.module('MotorcycleCtrl', []).controller('MotorcycleController', function
                 $scope.motorcycles = data;
 
                 $scope.brands = [];
+                $scope.models = [];
+                $scope.sizes = [];
+                $scope.purposes = [];
+
                 angular.forEach($scope.motorcycles, function (motorcycle) {
                     $scope.brands.push(motorcycle.BrandName);
-                });
-                $scope.brands = $scope.brands.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
-
-                $scope.models = [];
-                angular.forEach($scope.motorcycles, function (motorcycle) {
                     $scope.models.push(motorcycle.ModelName);
-                });
-                $scope.models = $scope.models.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
-
-                $scope.sizes = [];
-                angular.forEach($scope.motorcycles, function (motorcycle) {
                     $scope.sizes.push(motorcycle.SizeName);
-                });
-                $scope.sizes = $scope.sizes.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
-
-                $scope.purposes = [];
-                angular.forEach($scope.motorcycles, function (motorcycle) {
                     $scope.purposes.push(motorcycle.PurposeName);
                 });
+
+                $scope.brands = $scope.brands.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
+                $scope.models = $scope.models.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
+                $scope.sizes = $scope.sizes.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
                 $scope.purposes = $scope.purposes.filter(function (item, i, ar) { return ar.indexOf(item) === i; })
 
                 console.log(data);
